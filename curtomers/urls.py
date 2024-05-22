4eada066285fffe87e1dfd4341040516b9484e22
Ludmil <ludmil@Ludmils-MacBook-Air.local>
2024-05-22 02:54:48 +0200

    meal

--- curtomers/urls.py
+++ curtomers/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from curtomers.views import CustomerSignupView, customer_add_order, customer_driver_location, customer_get_detais, customer_get_latest_order, customer_get_order_history, customer_get_restaurants, customer_update_profile
+from curtomers.views import CustomerSignupView, customer_add_order, customer_driver_location, customer_get_detais, customer_get_latest_order, customer_get_meals, customer_get_order_history, customer_get_restaurants, customer_update_profile
 
 
 urlpatterns = [
-
+    path('customer/meals/<int:restaurant_id>/', customer_get_meals),
     path('customer/order/add/', customer_add_order),
     path('customer/restaurants/', customer_get_restaurants),
     path('signup/', CustomerSignupView.as_view()),
