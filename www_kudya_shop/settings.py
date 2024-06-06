46ba8096d91d035aa100051e608504c4f5dd81a6
Ludmil <ludmil@Ludmils-MacBook-Air.local>
2024-06-07 00:09:12 +0200

    db

--- www_kudya_shop/settings.py
+++ www_kudya_shop/settings.py
@@ -95,34 +95,28 @@ WSGI_APPLICATION = 'www_kudya_shop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-#DATABASES = {
-#    'default': {
-#       'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'maindoagency',
- #       'USER': 'super',
- #       'PASSWORD': 'Maitland@2024',  # Replace with your actual password
- #       'HOST': 'maindoagency-3864.postgres.pythonanywhere-services.com',
- #       'PORT': '13864',
- #   }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'maindoagency',
+        'USER': 'super',
+        'PASSWORD': 'Maitland@2024',  # Replace with your actual password
+        'HOST': 'maindoagency-3864.postgres.pythonanywhere-services.com',
+        'PORT': '13864',
     }
 }
 
-#DATABASES = {
- #       'default': {
- #           'ENGINE': 'django.db.backends.postgresql',
- #           'NAME': 'kudya',
-  #          'USER': 'super',
-  #          'PASSWORD': 'Maitland@2024',  # Replace with your actual password
-  #          'HOST': 'maindoagency-3864.postgres.pythonanywhere-services.com',
-  #          'PORT': '13864',
-  #      }
- #   }
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'kudya',
+            'USER': 'super',
+            'PASSWORD': 'Maitland@2024',  # Replace with your actual password
+            'HOST': 'maindoagency-3864.postgres.pythonanywhere-services.com',
+            'PORT': '13864',
+        }
+    }
 
 
 
