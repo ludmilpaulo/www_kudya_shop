dcb828f0571fd7093f69902a268a9f8c6a987e45
Ludmil <ludmil@Ludmils-MacBook-Air.local>
2024-07-17 16:21:14 +0200

    last

--- order/serializers.py
+++ order/serializers.py
@@ -77,4 +77,5 @@ class OrderSerializer(serializers.ModelSerializer):
             "proof_of_payment_restaurant",
             "proof_of_payment_driver",
             "payment_status_restaurant",
+            "original_price",
         )
