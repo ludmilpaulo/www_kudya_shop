3882bec4e5ac231af333acc215a8aa4679ac30ba
Ludmil <ludmil@Ludmils-MacBook-Air.local>
2025-04-30 23:18:51 +0200

    new

--- www_kudya_shop/settings.py
+++ www_kudya_shop/settings.py
@@ -102,10 +102,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'www_kudya_shop.wsgi.application'
 
 
+from decouple import config
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
